d95b0f8b7af3a91d98f8d329704123aa32ca0533
SaurPuss <saurlemons@gmail.com>
2019-11-13 15:14:40 +0100

    future command

---
+++ Lemonaid_1_13_2/src/main/java/me/saurpuss/lemonaid/commands/admin/duty/Exp.java
@@ -0,0 +1,10 @@
+package me.saurpuss.lemonaid.commands.admin.duty;
+
+public class Exp {
+
+    // TODO give exp amount
+    // TODO set exp amount
+    // TODO remove exp amount
+    // TODO set exp level
+    // TODO remove exp level
+}
